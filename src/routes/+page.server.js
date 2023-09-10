8459c7c1e35587a62cf523bb11ecc7ea64648c49
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-10 10:03:13 +0300

    feat: return existing shortpath if already present

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,14 +1,25 @@
+import { client } from '$lib/db/dbClient';
+
 /** @type {import('./$types').Actions} */
 export const actions = {
-  shortLink: async ({ request, url }) => {
-    const data = await request.formData();
+	shortLink: async ({ request, url }) => {
+		const data = await request.formData();
 
-    const responce = await fetch(`${url.origin}/api/shortLink`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url: data.get('url') })
-    });
+		const existingShortLink = await client.execute({
+			sql: 'select (shortpath) from links where url = :url limit 1',
+			args: { url: data.get('url')?.toString() ?? '' }
+		});
 
-    return await responce.json();
-  }
+    if (existingShortLink.rows.length) {
+      return existingShortLink.rows[0];
+    }
+
+		const responce = await fetch(`${url.origin}/api/shortLink`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ url: data.get('url') })
+		});
+
+		return await responce.json();
+	}
 };
