33f2a651647f4baf137b4743c1e0af1ecb99f16b
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-18 09:09:15 +0200

    feat: add usage column

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 create table links (
     id        integer primary key autoincrement,
-    url       text not null unique,
-    shortpath text not null unique
+    url       text    not null unique,
+    shortpath text    not null unique,
+    usage     integer not null default 0
 );
 
